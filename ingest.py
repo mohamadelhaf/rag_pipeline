38a651f2399ec07d3ebadec8693bf48a37607f66
mohamadelhaf <mhd_elhaf659@outlook.com>
2026-03-30 15:35:55 +0200

    modified ingest.py to support pdf files

--- ingest.py
+++ ingest.py
@@ -1,17 +1,42 @@
 from dotenv import load_dotenv
 import os
-from langchain_community.document_loaders import DirectoryLoader, TextLoader
+import sys
+import glob
+import shutil
+from langchain_community.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
 
 load_dotenv()
 
-def ingest_docs():
-    print("loading documents")
-    loader = DirectoryLoader("data/documents", glob="**/*.txt", show_progress=True, loader_cls=TextLoader)
-    documents = loader.load()
-    print(f"loaded {len(documents)} documents")
+def load_documents(directory):
+    all_docs =[]
+
+    for txt_file in glob.glob(f'{directory}/**/*.txt', recursive=True):
+        loader = TextLoader(txt_file)
+        all_docs.extend(loader.load())
+
+    for pdf_file in glob.glob(f'{directory}/**/*.pdf', recursive=True):
+        loader = PyPDFLoader(pdf_file)
+        all_docs.extend(loader.load())
+
+    return all_docs
+
+
+def ingest_docs(reset=False):
+
+    chroma_path = os.getenv("CHROMA_DB_PATH")
+
+    if reset and os.path.exists(chroma_path):
+        print(f'Resetting ChromaFB at {chroma_path}')
+        shutil.rmtree(chroma_path)
+        print("Cleared existing ChromaDB")
+
+    print("Loading documents")
+    documents = load_documents("data/documents")
+    print(f"Loaded {len(documents)} documents")
+
 
     print("splitting documents into chunks")
     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
@@ -27,4 +52,5 @@ def ingest_docs():
 
 
 if __name__ == "__main__":
-    ingest_docs()
+    reset = "--reset" in sys.argv
+    ingest_docs(reset=reset)
